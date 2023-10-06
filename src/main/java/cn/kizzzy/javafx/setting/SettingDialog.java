ee4f0fcbf3b10acee606ab01b29cf33599c8579a
Kizzzy <xuhua0174@163.com>
2023-10-07 01:36:42 +0800

    [javafx] - setting dialog remove recursively

--- src/main/java/cn/kizzzy/javafx/setting/SettingDialog.java
+++ src/main/java/cn/kizzzy/javafx/setting/SettingDialog.java
@@ -57,7 +57,6 @@ public class SettingDialog extends SettingDialogView implements Initializable, S
         public Object target;
         public SettingConfigs configs;
         public Consumer<ResultType> callback;
-        public Class<?> endClass;
     }
     
     private Args args;
@@ -121,72 +120,65 @@ public class SettingDialog extends SettingDialogView implements Initializable, S
         SettingGroup childHolder = null;
         
         Class<?> clazz = target.getClass();
-        while (clazz != null && !clazz.equals(Object.class)) {
-            Field[] fields = clazz.getDeclaredFields();
-            for (Field field : fields) {
-                if (Modifier.isStatic(field.getModifiers()) || Modifier.isFinal(field.getModifiers())) {
-                    continue;
-                }
-                
-                SettingConfig config = null;
-                if (configs != null) {
-                    config = configs.selectById(clazz, field.getName());
-                    if (config != null && config.ignore) {
-                        return;
-                    }
-                }
-                
-                if (childHolder == null) {
-                    childHolder = new SettingGroup();
-                    childHolder.setLabel(clazz.getSimpleName());
-                    
-                    root.getChildren().add(childHolder);
-                    
-                    AnchorPane.setLeftAnchor(childHolder, 0d);
-                    AnchorPane.setTopAnchor(childHolder, 0d);
-                    AnchorPane.setRightAnchor(childHolder, 0d);
-                    AnchorPane.setBottomAnchor(childHolder, 0d);
-                }
-                
-                Class<?> fieldType = field.getType();
-                
-                boolean found = false;
-                for (IFieldParser parser : parsers) {
-                    if (parser.accept(fieldType)) {
-                        Node node = parser.createNode(fieldType, field, target);
-                        if (node != null) {
-                            found = true;
-                            
-                            SettingItem item = new SettingItem();
-                            item.setLabel(config == null || config.alias == null ? field.getName() : config.alias);
-                            item.addChild(node);
-                            
-                            childHolder.addChild(item);
-                            break;
-                        }
-                    }
-                }
-                if (!found) {
-                    try {
-                        field.setAccessible(true);
-                        Object temp = field.get(target);
-                        if (temp == null) {
-                            temp = fieldType.newInstance();
-                            field.set(target, temp);
-                        }
-                        show(childHolder.getRoot(), temp, configs);
-                    } catch (Exception e) {
-                        LogHelper.error(null, e);
-                    } finally {
-                        field.setAccessible(false);
-                    }
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            if (Modifier.isStatic(field.getModifiers()) || Modifier.isFinal(field.getModifiers())) {
+                continue;
+            }
+            
+            SettingConfig config = null;
+            if (configs != null) {
+                config = configs.selectById(clazz, field.getName());
+                if (config != null && config.ignore) {
+                    return;
                 }
             }
             
-            if (clazz.equals(args.endClass)) {
-                break;
+            if (childHolder == null) {
+                childHolder = new SettingGroup();
+                childHolder.setLabel(clazz.getSimpleName());
+                
+                root.getChildren().add(childHolder);
+                
+                AnchorPane.setLeftAnchor(childHolder, 0d);
+                AnchorPane.setTopAnchor(childHolder, 0d);
+                AnchorPane.setRightAnchor(childHolder, 0d);
+                AnchorPane.setBottomAnchor(childHolder, 0d);
             }
-            clazz = clazz.getSuperclass();
+            
+            Class<?> fieldType = field.getType();
+            
+            boolean found = false;
+            for (IFieldParser parser : parsers) {
+                if (parser.accept(fieldType)) {
+                    Node node = parser.createNode(fieldType, field, target);
+                    if (node != null) {
+                        found = true;
+                        
+                        SettingItem item = new SettingItem();
+                        item.setLabel(config == null || config.alias == null ? field.getName() : config.alias);
+                        item.addChild(node);
+                        
+                        childHolder.addChild(item);
+                        break;
+                    }
+                }
+            }
+            if (!found) {
+                try {
+                    field.setAccessible(true);
+                    Object temp = field.get(target);
+                    if (temp == null) {
+                        temp = fieldType.newInstance();
+                        field.set(target, temp);
+                    }
+                    show(childHolder.getRoot(), temp, configs);
+                } catch (Exception e) {
+                    LogHelper.error(null, e);
+                } finally {
+                    field.setAccessible(false);
+                }
+            }
         }
     }
 }
