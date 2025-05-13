971ed961866b46aae33b5ff9a33e6b5f48c8226b
Kizzzy <xuhua0174@163.com>
2025-05-13 11:35:18 +0800

    [javafx] * refactor LabeledChooser and SettingGroupView

--- src/main/java/cn/kizzzy/javafx/setting/parser/StringFieldParserImpl.java
+++ src/main/java/cn/kizzzy/javafx/setting/parser/StringFieldParserImpl.java
@@ -2,14 +2,17 @@ package cn.kizzzy.javafx.setting.parser;
 
 import cn.kizzzy.config.Password;
 import cn.kizzzy.config.Path;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.input.TransferMode;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
@@ -27,8 +30,12 @@ public abstract class StringFieldParserImpl<T> extends AbstractFieldParser<Strin
     public Node createNode(final FieldParserFactory factory, final Class<?> clazz, final Field field, final Object target) {
         String value = getValue(field, target);
         
-        HBox hBox = new HBox();
-        hBox.setSpacing(8);
+        StackPane stackPane = new StackPane();
+        stackPane.getStylesheets().add(getClass().getResource("/css/button.css").toExternalForm());
+        AnchorPane.setTopAnchor(stackPane, 0.0);
+        AnchorPane.setRightAnchor(stackPane, 0.0);
+        AnchorPane.setBottomAnchor(stackPane, 0.0);
+        AnchorPane.setLeftAnchor(stackPane, 0.0);
         
         Password password = field.getAnnotation(Password.class);
         
@@ -49,12 +56,12 @@ public abstract class StringFieldParserImpl<T> extends AbstractFieldParser<Strin
             }
         });
         
-        hBox.getChildren().add(textField);
-        HBox.setHgrow(textField, Priority.ALWAYS);
+        stackPane.getChildren().add(textField);
         
         Path path = field.getAnnotation(Path.class);
         if (path != null) {
             Button button = new Button("浏览");
+            button.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
             button.setOnAction(event -> {
                 if (path.file()) {
                     FileChooser chooser = new FileChooser();
@@ -71,9 +78,12 @@ public abstract class StringFieldParserImpl<T> extends AbstractFieldParser<Strin
                 }
             });
             
-            hBox.getChildren().add(button);
+            stackPane.getChildren().add(button);
+            
+            StackPane.setAlignment(button, Pos.CENTER_RIGHT);
+            StackPane.setMargin(button, new Insets(0, 8, 0, 0));
         }
         
-        return hBox;
+        return stackPane;
     }
 }
